02611e9827f6dd3a813a716de36f7cbfbbea37cf
FrancoisDoue <doue.francois@outlook.com>
2024-09-09 17:26:59 +0200

    security start

---
+++ authentication-service/src/main/java/com/example/authenticationservice/config/jwt/JwtProvider.java
@@ -0,0 +1,67 @@
+package com.example.authenticationservice.config.jwt;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+public class JwtProvider {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Base64.getDecoder().decode(secret.getBytes());
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(Authentication authentication){
+        String username = authentication.getName();
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + 1000 * 60 * 60 * 24);
+
+        String roles = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority).collect(Collectors.joining(","));
+
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("roles", roles)
+//                .claim("id", ((User) authentication.getPrincipal()).getId())
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(getSigningKey())
+                .compact();
+
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            throw new AuthenticationCredentialsNotFoundException("Invalid token");
+        }
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+
+}
